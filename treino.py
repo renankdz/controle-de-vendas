b4d9bcc789bbcf21f4be56630c759f51dfe2aa83
Renan <renanmeneses148@gmail.com>
2026-03-10 10:51:39 -0300

    Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is ahead of 'origin/main' by 2 commits. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   index.html #	modified:   lista.html #	modified:   script.js #	modified:   styles.css #	deleted:    treino.py #

--- treino.py
+++
@@ -1,7 +0,0 @@
-entrada = [1, -2, "3", None, 5, 0, 8]
-def filtrar_valores_validos(lista):
-    total = 0
-    for item in lista:
-        if type(item) == int and item > 0:
-            total += item
-    return total
